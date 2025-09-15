1e14aa9d89931cd28af171834c06c2dcba6f2790
Onabeitia <onabeitia@icloud.com>
2025-09-15 11:31:48 -0300

    elimine extras

--- peliculas/src/screens/Movies.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const API = 'https://api.themoviedb.org/3';
-const API_KEY = 'PON_TU_API_KEY_ACA';
-const IMG = 'https://image.tmdb.org/t/p/';
-
