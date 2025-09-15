9dfd78fafbd7769b0d87d5a8b02e8f1f1e9dcbc0
felichiloteguy <fchiloteguy@udesa.edu.ar>
2025-09-15 13:19:10 -0300

    integrando api

--- peliculas/src/screens/Movies.js
+++ peliculas/src/screens/Movies.js
@@ -8,7 +8,7 @@ class Movies extends Component {
         }
     }
     componentDidMount() {
-        fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1')
+        fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=04e6a27eeae7267e69af197c8db319ff')
         .then( response => response.json)
         .then(data => this.setState(
             {
@@ -16,6 +16,15 @@ class Movies extends Component {
             }
         ))
         .catch(error => console.log(error))
+    }
+    render() {
+        return (
+            <div>
+                {this.state.peliculas.map(movie => (
+                    <p key={movie.id}>{movie.title}</p>
+                ))}
+            </div>
+        )
     }
 }
 
